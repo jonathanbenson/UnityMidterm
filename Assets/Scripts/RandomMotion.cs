b083c7394bbeb0b4639b2189d6f026be055d78f8
Jonathan Benson <jbensonamdg@gmail.com>
2022-03-19 20:51:14 -0500

    Added many comments.

--- Assets/Scripts/RandomMotion.cs
+++ Assets/Scripts/RandomMotion.cs
@@ -3,23 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomMotion : MonoBehaviour
+// Handles the motion of the asteroids in the asteroid field
+// Each of their velocities are generated randomly
 {
 
+    // x and z speed components of game object
     float xSpeed, zSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
+        // initialize the x and z speed components with a random, LOW value
         xSpeed = Random.Range(-.25f, .25f) * Time.deltaTime;
         zSpeed = Random.Range(-.25f, .25f) * Time.deltaTime;
-
-
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        // move each game object each frame according to the speed of the object
         transform.Translate(new Vector3(xSpeed, 0, zSpeed));
     }
 }
--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -3,35 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpaceshipController : MonoBehaviour
+// Controller to move the spaceship according to the keys that the user presses (WASD)
 {
-
+	
+	// variable to keep track of the speed of the spaceship
 	public float speed = 5;
 
 	// Update is called once per frame
 	void Update() {
 
+		// initialize the offset value for the speed to 0
 		Vector3 offset = Vector3.zero;
 
+		// retrieve the x and z speed components from the input manager
 		offset.x = Input.GetAxis("Horizontal");
 		offset.z = Input.GetAxis("Vertical");
 
+		// calculate the next offset as the product of the current offset and speed
+		// ...also multiply the next offset by the time delta to make the motion smooth across all architectures
 		offset = offset * speed * Time.deltaTime;
 
+		// update the position of the spaceship
 		transform.position = transform.position + offset;
 	}
 
 	void OnCollisionEnter(Collision collision) {
 
+		// upon the spaceship colliding with a game object
+		// tell the user they lost in the debug console
 		Debug.Log("You Lose!");
 
+		// destroy the spaceship (makes it disappear)
 		Destroy(this.gameObject);
 
 	}
 
 	private void OnTriggerEnter(Collider other) {
 
-		Debug.Log("You win!");
+		// upon the spaceship making it to the end of the asteroid field
+		// tell the user they won in the debug console
+		Debug.Log("You Win!");
 
+		// destroy the spaceship (makes it disappear)
 		Destroy(this.gameObject);
 
 	}
