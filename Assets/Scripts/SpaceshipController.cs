0256df499d2c9f6207da7ca1cef700e142abbafe
Jonathan Benson <jbensonamdg@gmail.com>
2022-03-19 19:12:49 -0500

    Player is able to win match by making it to the end of the asteroid field.

--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -8,8 +8,8 @@ public class SpaceshipController : MonoBehaviour
 	public float speed = 5;
 
 	// Update is called once per frame
-	void Update()
-	{
+	void Update() {
+
 		Vector3 offset = Vector3.zero;
 
 		offset.x = Input.GetAxis("Horizontal");
@@ -18,7 +18,21 @@ public class SpaceshipController : MonoBehaviour
 		offset = offset * speed * Time.deltaTime;
 
 		transform.position = transform.position + offset;
+	}
 
+	void OnCollisionEnter(Collision collision) {
+
+		Debug.Log("You Lose!");
+
+		Destroy(this.gameObject);
+
+	}
+
+	private void OnTriggerEnter(Collider other) {
+
+		Debug.Log("You win!");
+
+		Destroy(this.gameObject);
 
 	}
 }
